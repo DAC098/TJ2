a221aad5e883305793e4ddce1ec4f0e18cd0288d
dac098 <me@dac098.me>
2024-11-28 03:09:51 -0800

    updated entries table
    updated the entries table to use the table structure from shadcn

--- frontend/src/Entries.tsx
+++ frontend/src/Entries.tsx
@@ -1,6 +1,11 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+import {
+    DataTable,
+    ColumnDef,
+} from "@/components/ui/table";
+
 interface JournalEntry {
     id: number,
     date: string,
@@ -13,6 +18,52 @@ interface JournalTags {
     [key: string]: string | null
 }
 
+async function retrieve_entries() {
+    let res = await fetch("/entries");
+
+    if (res.status !== 200) {
+        return null;
+    }
+
+    return await res.json() as JournalEntry[];
+}
+
+const columns: ColumnDef<JournalEntry>[] = [
+    {
+        accessorKey: "date",
+        header: "Date",
+        cell: ({ row }) => {
+            let date = row.original.date;
+
+            return <Link to={`/entries/${date}`}>{date}</Link>;
+        }
+    },
+    {
+        accessorKey: "title",
+        header: "Title",
+    },
+    {
+        accessorKey: "tags",
+        header: "Tags",
+        cell: ({ row }) => {
+            let list = [];
+
+            for (let tag in row.original.tags) {
+                list.push(<span key={tag}>{tag}</span>);
+            }
+
+            return <>{list}</>;
+        }
+    },
+    {
+        accessorKey: "mod",
+        header: "Mod",
+        cell: ({ row }) => {
+            return row.original.updated != null ? row.original.updated : row.original.created;
+        }
+    }
+];
+
 const Entries = () => {
     let [loading, setLoading] = useState(false);
     let [entries, setEntries] = useState<JournalEntry[]>([]);
@@ -22,79 +73,22 @@ const Entries = () => {
 
         setLoading(true);
 
-        fetch("/entries").then(res => {
-            if (res.status !== 200) {
-                console.log("non 200 response status:", res);
-
-                return;
-            }
-
-            let content_type = res.headers.get("content-type");
-
-            if (content_type == null || content_type !== "application/json") {
-                console.log("unknown or invalid content-type");
-
-                return;
-            }
-
-            return res.json();
-        }).then(json => {
-            if (json == null) {
-                return;
-            }
-
+        retrieve_entries().then(json => {
             setEntries(() => {
-                return json as JournalEntry[];
+                return json;
             });
         }).catch(err => {
-            console.log("failed to retrieve entries:", err);
+            console.error("failed to retrieve entries:", err);
         }).finally(() => {
             setLoading(false);
         });
     }, []);
 
-    let entry_rows = [];
-
-    for (let entry of entries) {
-        let tags = [];
-
-        for (let tag in entry.tags) {
-            tags.push(<span key={tag}>{tag}</span>);
-        }
-
-        let mod = entry.updated != null ? entry.updated : entry.created;
-
-        entry_rows.push(<tr key={entry.id}>
-            <td>
-                <Link to={`/entries/${entry.date}`}>{entry.date}</Link>
-            </td>
-            <td>{entry.title}</td>
-            <td>{tags}</td>
-            <td>{mod}</td>
-        </tr>);
-    }
-
-    if (entry_rows.length === 0) {
-        entry_rows.push(<tr key={0}>
-            <td colSpan={4}>No Entries</td>
-        </tr>);
-    }
-
     if (loading) {
         return <div>loading entries</div>
     } else {
         return <div>
-            <table>
-                <thead>
-                    <tr className="sticky top-0 bg-white">
-                        <th>Date</th>
-                        <th>Title</th>
-                        <th>Tags</th>
-                        <th>Mod</th>
-                    </tr>
-                </thead>
-                <tbody>{entry_rows}</tbody>
-            </table>
+            <DataTable columns={columns} data={entries}/>
         </div>
     }
 };
