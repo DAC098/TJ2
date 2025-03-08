6632140e35b243ee38311ca561edecbdc26dfd83
dac098 <me@dac098.me>
2025-03-08 13:23:16 -0800

    custom fields synced #6
    updated the entries sync to handle custom fields data that it receives.
    fairly straight forward but still requires manually specifying the field
    uids.

--- src/router/sync.rs
+++ src/router/sync.rs
@@ -1,14 +1,14 @@
+use std::default::Default;
+
 use axum::Router;
-use axum::extract::Path;
-use axum::http::{StatusCode, Uri, HeaderMap};
-use axum::response::{IntoResponse, Response};
 use axum::routing::post;
-use chrono::{Utc, DateTime};
-use futures::StreamExt;
-use serde::{Serialize, Deserialize};
 
 use crate::db;
-use crate::db::ids::EntryId;
+use crate::db::ids::{
+    JournalId,
+    EntryId,
+    CustomFieldUid,
+};
 use crate::error::{self, Context};
 use crate::router::body;
 use crate::journal;
@@ -68,6 +68,25 @@ async fn receive_entry(
     let entries_id: EntryId = result.get(0);
 
     upsert_tags(&transaction, &entries_id, &json.tags).await?;
+
+    {
+        let UpsertCFS {
+            not_found,
+            invalid
+        } = upsert_cfs(&transaction, &journal.id, &entries_id, &json.custom_fields).await?;
+
+        if !not_found.is_empty() {
+            return Ok(SyncEntryResult::CFNotFound {
+                uids: not_found
+            });
+        }
+
+        if !invalid.is_empty() {
+            return Ok(SyncEntryResult::CFInvalid {
+                uids: invalid
+            });
+        }
+    }
 
     transaction.commit()
         .await
@@ -132,4 +151,89 @@ async fn upsert_tags(
     }
 
     Ok(())
+}
+
+struct UpsertCFS {
+    not_found: Vec<CustomFieldUid>,
+    invalid: Vec<CustomFieldUid>,
+}
+
+impl Default for UpsertCFS {
+    fn default() -> Self {
+        UpsertCFS {
+            not_found: Vec::new(),
+            invalid: Vec::new(),
+        }
+    }
+}
+
+async fn upsert_cfs(
+    conn: &impl db::GenericClient,
+    journals_id: &JournalId,
+    entries_id: &EntryId,
+    cfs: &Vec<sync::journal::EntryCFSync>,
+) -> Result<UpsertCFS, error::Error> {
+    let mut results = UpsertCFS::default();
+
+    if !cfs.is_empty() {
+        let fields = journal::CustomField::retrieve_journal_uid_map(conn, journals_id)
+            .await
+            .context("failed to retrieve journal custom fields")?;
+        let mut params: db::ParamsVec<'_> = vec![entries_id];
+        let mut query = String::from(
+            "\
+            with tmp_insert as ( \
+                insert into custom_field_entries (entries_id, custom_fields_id, value, created, updated) \
+                values "
+        );
+
+        for (index, cf) in cfs.iter().enumerate() {
+            let Some(field_ref) = fields.get(&cf.custom_fields_uid) else {
+                results.not_found.push(cf.custom_fields_uid.clone());
+
+                continue;
+            };
+
+            if index != 0 {
+                query.push_str(", ");
+            }
+
+            let statement = format!(
+                "($1, ${}, ${}, ${}, ${})",
+                db::push_param(&mut params, &field_ref.id),
+                db::push_param(&mut params, &cf.value),
+                db::push_param(&mut params, &cf.created),
+                db::push_param(&mut params, &cf.updated),
+            );
+
+            query.push_str(&statement);
+        }
+
+        query.push_str(" on conflict (custom_fields_id, entries_id) do update \
+                set value = excluded.value, \
+                    updated = excluded.updated \
+                returning custom_fields_id, \
+                          entries_id \
+            )
+            delete from custom_field_entries \
+            using tmp_insert \
+            where custom_field_entries.entries_id = tmp_insert.entries_id and \
+                  custom_field_entries.custom_fields_id != tmp_insert.custom_fields_id"
+        );
+
+        conn.execute(&query, params.as_slice())
+            .await
+            .context("failed to upsert custom fields")?;
+    } else {
+        conn.execute(
+            "\
+            delete from custom_field_entries \
+            where entries_id = $1",
+            &[entries_id]
+        )
+            .await
+            .context("failed to delete custom fields")?;
+    }
+
+    Ok(results)
 }
