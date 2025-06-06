745e6d573530ef1b0dcd5fef0099fc0d62f66bb3
dac098 <me@dac098.me>
2025-06-05 20:17:41 -0700

    fixed json content-type check
    since the updated json body from the server includes parameters in the
    content-type, the res_as_json failed when it should pass. included new
    module to parse mime types and properly checking the mime if its json.

--- frontend/src/net.ts
+++ frontend/src/net.ts
@@ -1,11 +1,17 @@
+import MIMEType from "whatwg-mimetype";
+
 export async function res_as_json<T>(res: Response): Promise<T> {
     let content_type = res.headers.get("content-type");
 
-    if (content_type == null || content_type !== "application/json") {
+    if (content_type == null) {
         throw new Error("unspecified content-type from response");
     }
 
-    if (content_type !== "application/json") {
+    let mime = new MIMEType(content_type);
+
+    console.log(mime);
+
+    if (mime.type !== "application" && mime.subtype !== "json") {
         throw new Error("non json content-type");
     }
 
@@ -13,6 +19,7 @@ export async function res_as_json<T>(res: Response): Promise<T> {
 }
 
 type RequestMethod = "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH";
+
 interface RequestOptions {
     headers?: {[header: string]: string}
 }
